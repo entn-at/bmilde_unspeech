b17b32983d408f0ba079a147b546a471ae040d3e
Benjamin Milde <me@anon-63-83.vpn.ipredator.se>
2017-06-26 15:21:09 +0200

    corrected debug

--- unsup_model_neg.py
+++ unsup_model_neg.py
@@ -443,7 +443,7 @@ def train(filelist):
 
                     #print('input_window_1:', input_window_1[0])
                     #print('input_window_2:', input_window_2[0])
-                    print('out (first 20 dims):', out[0][:20])
+                    print('out (first 40 dims):', out[:40])
                     print('At step %i step-time %.4f loss %.4f' % (current_step, step_time, mean_train_loss))
                     
                     train_losses = []
