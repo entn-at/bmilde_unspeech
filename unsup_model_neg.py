1d93f9e74d39b221a0397f2591d261cdd860e574
shapecatcher <none>
2017-07-21 18:17:43 +0200

    scipy for loading

--- unsup_model_neg.py
+++ unsup_model_neg.py
@@ -539,7 +539,7 @@ def gen_feat(filelist, feats_outputfile, feats_format):
                     
                     window_length_seconds = float(FLAGS.window_length)/float(FLAGS.sample_rate)
                     model_params = ('e2e' if FLAGS.end_to_end else '') + '_trans' + FLAGS.embedding_transformation + '_win' + str(FLAGS.window_length) + '_lcontexts' + str(FLAGS.left_contexts) + '_rcontexts' + str(FLAGS.right_contexts) + \
-                                    '_flts' + str(FLAGS.num_filters) + '_embsize' + str(FLAGS.fc_size) + ('dnn' + str(FLAGS.num_dnn_layers) if FLAGS.embedding_transformation=='BaselineDnn' else '') + \
+                                    '_flts' + str(FLAGS.num_filters) + '_embsize' + str(FLAGS.embedding_size) + ('dnn' + str(FLAGS.num_dnn_layers) if FLAGS.embedding_transformation=='BaselineDnn' else '') + \
                                     ('highwaydnn' + str(FLAGS.num_highway_layers) if FLAGS.embedding_transformation=='HighwayDnn' else '') + \
                                     ('dot_combine' if FLAGS.dot_combine else '')
                     
