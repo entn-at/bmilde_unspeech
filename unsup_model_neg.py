209f018d8eb52dfc16bc4fa8e4e84cb91e11a1c9
Benjamin Milde <milde@ltmac9.informatik.uni-hamburg.de>
2017-08-03 18:33:16 +0200

    disconnect embeddings size from fcsize, added log1p

--- unsup_model_neg.py
+++ unsup_model_neg.py
@@ -31,7 +31,11 @@ tf.flags.DEFINE_string("filter_sizes", "512", "Comma-separated filter sizes (def
 tf.flags.DEFINE_integer("num_filters", 40, "Number of filters per filter size (default: 40)")
 tf.flags.DEFINE_integer("window1_length", 1024, "First window length, samples or frames") # 100+ ms @ 16kHz
 tf.flags.DEFINE_integer("window2_length", 1024, "Second window length, samples or frames") # 100+ ms @ 16kHz
-tf.flags.DEFINE_integer("embedding_size", 256 , "Fully connected size at the end of the network.")
+tf.flags.DEFINE_integer("embeddings_size", 256 , "Size of the generated embeddings.")
+tf.flags.DEFINE_integer("fc_size", 1024 , "Fully connected size at the end of the network.")
+
+tf.flags.DEFINE_boolean("first_layer_tanh", True, "Whether tanh should be used for the output conv1d filters in end-to-end networks.")
+tf.flags.DEFINE_boolean("first_layer_log1p", True, "Whether log1p should be applied to the output of the conv1d filters.")
 
 tf.flags.DEFINE_boolean("with_vgg16", False, "Whether to use a vgg16 network for the embeddings computation.")
 tf.flags.DEFINE_boolean("with_dense_network", False,  "Whether to use a dense conv network for the embeddings computation.")
@@ -284,13 +288,14 @@ class UnsupSeech(object):
         return window1_batch,window2_batch,labels
     
     
-    def __init__(self, window_size_1, window_size_2, filter_sizes, num_filters, fc_size, dropout_keep_prob, train_files, k, is_training=True, create_new_train_dir=True, batch_size=128):
+    def __init__(self, window_size_1, window_size_2, filter_sizes, num_filters, fc_size, embeddings_size, dropout_keep_prob, train_files, k, is_training=True, create_new_train_dir=True, batch_size=128):
 
         self.train_files = train_files
 
         self.window_size_1 = window_size_1
         self.window_size_2 = window_size_2
         self.fc_size = fc_size
+        self.embeddings_size = embeddings_size
 
         # None -> automatically sets the dimension to batch_size
         # window 1 is fixed
@@ -364,7 +369,14 @@ class UnsupSeech(object):
         
                         ## Apply nonlinearity
                         b = tf.Variable(tf.constant(0.01, shape=[num_filters]), name="bias1")
-                        conv = tf.nn.tanh(tf.nn.bias_add(conv, b), name="activation1")
+                        
+                        if FLAGS.first_layer_tanh:
+                            conv = tf.nn.tanh(tf.nn.bias_add(conv, b), name="activation1")
+                        else:
+                            conv = lrelu(tf.nn.bias_add(conv, b), name="activation1")
+        
+                        if FLAGS.first_layer_log1p:
+                            conv = tf.log1p(tf.abs(conv))
         
                         pool_input_dim = int(conv.get_shape()[1])
         
@@ -427,7 +439,7 @@ class UnsupSeech(object):
                                                 #normalizer_fn=slim.batch_norm if FLAGS.batch_normalization else None,
                                                 #normalizer_params={'is_training': is_training, 'decay': 0.95} if FLAGS.batch_normalization else None):
                             for x in range(FLAGS.num_dnn_layers):
-                                self.flattened_pooled = slim.fully_connected(self.flattened_pooled, fc_size*4)                    
+                                self.flattened_pooled = slim.fully_connected(self.flattened_pooled, self.fc_size)                    
                    
                         #with tf.variable_scope('visualization_embedding'):
                         #    flattened_pooled_normalized = utils.tensor_normalize_0_to_1(self.flattened_pooled)
@@ -435,7 +447,7 @@ class UnsupSeech(object):
         
                         print('flattened_pooled shape:',self.flattened_pooled.get_shape())
         
-                        self.fc1 = slim.fully_connected(self.flattened_pooled, fc_size)#weights_initializer=tf.truncated_normal_initializer(stddev=0.01)) #is_training)
+                        self.fc1 = slim.fully_connected(self.flattened_pooled, self.fc_size)#weights_initializer=tf.truncated_normal_initializer(stddev=0.01)) #is_training)
                         print('fc1 shape:',self.fc1.get_shape())
                         self.outs.append(self.fc1)
                         
@@ -468,7 +480,7 @@ def train(filelist):
     with tf.device('/gpu:1'):
         with tf.Session(config=tf.ConfigProto(allow_soft_placement=True, log_device_placement=False)) as sess:
             model = UnsupSeech(window_size_1=FLAGS.window1_length, window_size_2=FLAGS.window2_length, filter_sizes=filter_sizes, 
-                                num_filters=FLAGS.num_filters, fc_size=FLAGS.embedding_size, dropout_keep_prob=FLAGS.dropout_keep_prob, k = FLAGS.negative_samples ,train_files = filelist,  batch_size=FLAGS.batch_size)
+                                num_filters=FLAGS.num_filters, fc_size=FLAGS.fc_size, embeddings_size = FLAGS.embeddings_size, dropout_keep_prob=FLAGS.dropout_keep_prob, k = FLAGS.negative_samples ,train_files = filelist,  batch_size=FLAGS.batch_size)
             
             restored = False
             if FLAGS.train_dir != "":
