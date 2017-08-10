9ff2aac341ee587d86c7bf28d0e88f1f41dc4c6e
Benjamin Milde <milde@ltgpu1.informatik.uni-hamburg.de>
2017-08-10 20:52:47 +0200

    typo

--- unsup_model_neg.py
+++ unsup_model_neg.py
@@ -644,7 +644,7 @@ def train(utt_id_list, spk2utt=None, spk2len=None, num_speakers=None):
     with tf.device('/gpu:1'):
         with tf.Session(config=tf.ConfigProto(allow_soft_placement=True, log_device_placement=False)) as sess:
             model = UnsupSeech(window_length=FLAGS.window_length, window_neg_length=FLAGS.window_neg_length, filter_sizes=filter_sizes, 
-                                num_filters=FLAGS.num_filters, fc_size=FLAGS.fc_size, embeddings_size = FLAGS.embeddings_size, dropout_keep_prob=FLAGS.dropout_keep_prob, 
+                                num_filters=FLAGS.num_filters, fc_size=FLAGS.fc_size, embeddings_size = FLAGS.embedding_size, dropout_keep_prob=FLAGS.dropout_keep_prob, 
                                 k = FLAGS.negative_samples, left_contexts=FLAGS.left_contexts, right_contexts=FLAGS.right_contexts, train_files = utt_id_list,  batch_size=FLAGS.batch_size)
             
             training_start_time = time.time()
